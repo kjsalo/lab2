d9e8e8907c38ab4f6a6ae9ca90ec349c1ade22b5
Kari Salo <kjsalo@gmail.com>
2014-11-03 17:23:12 +0200

    skripti

--- readimg.js
+++ readimg.js
@@ -1,19 +1,7 @@
  $(document).on("pageinit", function(){ 
 	$("button").click(function() {
-		//navigator.device.capture.captureImage(captureSuccess, captureError, {limit: 1});
 		navigator.camera.getPicture(onSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI });
 	});
-	/*function captureSuccess(mediaFiles) {
-		$("#myimg").attr("src",mediaFiles[0].fullPath);    
-	}
-
-	function captureError(error) {
-        var msg = 'An error occurred during capture: ' + error.code;
-        navigator.notification.alert(msg, null, 'Uh oh!');
-	}	
-	
-	*/
-});
 
 function onSuccess(imageURI) {
     $("#myimg").attr("src", imageURI);
@@ -23,5 +11,8 @@ function onFail(message) {
     var msg = 'An error occurred during capture: ' + message;
     navigator.notification.alert(msg, alerting, 'Not good', 'Done');
 }
+
 function alerting(){
 }
+
+});
